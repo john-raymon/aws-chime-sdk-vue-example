6fd9f1c5722bb489162f15a90d253fba203bf1b8
John Ray <john@trychec.com>
2021-01-05 01:58:17 -0500

    connect and bind audio

--- client/src/pages/MeetingRoom.vue
+++ client/src/pages/MeetingRoom.vue
@@ -5,6 +5,7 @@
     </template>
     <template v-else-if="meetingSession && !error">
       <p>Meeting room</p>
+      <audio ref="audioElement"></audio>
     </template>
     <template v-else-if="error">
       <p class="mx-auto text-red-500">Oops, there was an error connecting you to this room.</p>
@@ -69,7 +70,56 @@ export default {
         deviceController
       );
       this.audioVideo = this.meetingSession.audioVideo;
-    }
+      this.setUpDevicesAndStart();
+    },
+    setUpDevicesAndStart() {
+      // observe audio-video lifecycle
+      const observer = {
+        audioVideoDidStart: () => {
+          console.log("Started");
+        },
+        audioVideoDidStop: (sessionStatus) => {
+          console.log("Stopped with a session status code: ", sessionStatus.statusCode());
+        },
+        audioVideoDidStartConnecting: (reconnecting) => {
+          if (reconnecting) {
+            console.log("Attempting to reconnect");
+          }
+        },
+      };
+      // set up audio input and out put devices, and bind to DOM audio element
+      this.audioVideo.listAudioInputDevices()
+        .then(audioInputDevices => {
+          debugger;
+          this.audioVideo.chooseAudioInputDevice(audioInputDevices[0].deviceId);
+          return this.audioVideo.listAudioOutputDevices();
+        })
+        .then(audioOutputDevices => {
+          debugger;
+          // note: on firefox browsers there audioOutputDevices returns as empty
+          // https://github.com/aws/amazon-chime-sdk-js/issues/657#issuecomment-687363939
+          return this.audioVideo.chooseAudioOutputDevice(audioOutputDevices.length ? audioOutputDevices[0].deviceId : null);
+        })
+        .then(() => {
+          const audioElement = this.$refs;
+          debugger;
+          // bind audio output to audio HTML DOM element using ref
+          this.audioVideo.bindAudioElement(audioElement);
+          debugger;
+          // register audio-video lifecycle observer
+          this.audioVideo.addObserver(observer);
+          debugger;
+          this.audioVideo.start();
+        })
+        .catch((err) => {
+          debugger;
+          console.log('error', err);
+          this.error = {
+            ...err,
+            message: 'There was an error setting up your audio inputs',
+          }
+        })
+    },
   }
 };
 </script>
